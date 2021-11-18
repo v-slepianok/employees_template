a82021461b26c188f2369676b76f44cbca502f34
Valeryia Slepianok <direnok.vd@mail.ru>
2021-11-18 23:19:54 +0200

    create add form

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -3,6 +3,7 @@ import AppInfo from '../app-info/app-info';
 import SearchPanel from '../search-panel/search-panel';
 import AppFilter from '../app-filter/app-filter';
 import EmployeesList from '../employees-list/employees-list'
+import EmployeesAddForm from '../employees-add-form/employees-add-form'; 
 
 import './app.css';
 
@@ -15,6 +16,7 @@ function App () {
                 <AppFilter/>
             </div>
             <EmployeesList/>
+            <EmployeesAddForm/>
         </div>
     );
 }
