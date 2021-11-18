f4acab57102e236f21807c3ce0bab9fb379c1176
Valeryia Slepianok <direnok.vd@mail.ru>
2021-11-18 23:18:00 +0200

    created enployees list

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -2,6 +2,8 @@
 import AppInfo from '../app-info/app-info';
 import SearchPanel from '../search-panel/search-panel';
 import AppFilter from '../app-filter/app-filter';
+import EmployeesList from '../employees-list/employees-list'
+
 import './app.css';
 
 function App () {
@@ -12,7 +14,7 @@ function App () {
                 <SearchPanel/>,
                 <AppFilter/>
             </div>
-    
+            <EmployeesList/>
         </div>
     );
 }
