ecea84af1e9eb3402bc7ac172b594b3c5bdd2a03
Valeryia Slepianok <direnok.vd@mail.ru>
2022-02-17 13:40:37 +0300

    added search method

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -15,7 +15,8 @@ class App extends Component{
                 {name: 'Mary Frezee', salary: 800, increase: false, rise: true, id: 1},
                 {name: 'John Smidt', salary: 1000, increase: true, rise: false, id:2 },
                 {name: 'Ann Grok', salary: 36000, increase: false, rise: false, id: 3},
-            ]
+            ],
+            term: ''
         }
         this.maxId = 4;
     }
@@ -56,11 +57,24 @@ class App extends Component{
         }))
     }
      
+    searchEmp  = (items, term) => {
+        if(items.length === 0){
+            return items;
+        }
+        return items.filter(item => {
+            return item.name.indexOf(term) > -1;
+        })
+    }
    
+    onUpdateSearch = (term) => {
+        this.setState({term});
+    }
 
     render() {
+        const {data, term} = this.state;
         const employees = this.state.data.length;
         const increased = this.state.data.filter(item => item.increase).length;
+        const visibleData = this.searchEmp(data, term);
         return(
             <div className="app">
                 <AppInfo
@@ -68,11 +82,11 @@ class App extends Component{
                 increased={increased}
                 />
                 <div className="search-panel">
-                    <SearchPanel/>
-                    <AppFilter/>
+                    <SearchPanel  onUpdateSearch={this.onUpdateSearch}/>
+                    <AppFilter />
                 </div>
                 <EmployeesList 
-                    data={this.state.data}
+                    data={visibleData}
                     onDelete={this.deleteItem}
                     onToggleProp={this.onToggleProp}/>
                 <EmployeesAddForm
